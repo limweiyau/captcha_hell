2454a5597d39d0d238953f94725b7c53e3d80600
Wei Yau Lim <limweiyau@axs.com.sg>
2025-08-07 17:27:00 +0800

    Save point 1 - add deepfried stage

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@
 import stage1 from "./stages/stage1.js";
 import stage2 from "./stages/stage2.js";
 import stage3 from "./stages/stage3.js";
+import stage0 from "./stages/stage0.js";
 
-const stages = [stage1, stage2, stage3]; // append new modules here
+const stages = [stage0, stage1, stage2, stage3]; // append new modules here
 
 /* ---------------- helpers ---------------- */
 const appEl   = document.getElementById("app");
